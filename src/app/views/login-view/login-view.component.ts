03c4b8888ce0ec8dfd6357d8ada28171ca25fd5d
Edycar Reyes <edycarreyes@gmail.com>
2020-11-01 15:11:38 -0600

    Finished the design for login-view

--- src/app/views/login-view/login-view.component.ts
+++ src/app/views/login-view/login-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-login-view',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginViewComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  // Funcion para login
+  login(event: any): void {
+    console.log('Doing login');
   }
 
 }
