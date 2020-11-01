2c9d1494022f5d6217c72976dd01f641a618c553
Edycar Reyes <edycarreyes@gmail.com>
2020-11-01 17:24:31 -0600

    Created the files for the Sidenav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginViewComponent } from './views/login-view/login-view.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { DashboardViewComponent } from './views/dashboard-view/dashboard-view.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginViewComponent} from './views/login-view/login-view.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {DashboardViewComponent} from './views/dashboard-view/dashboard-view.component';
+import {SidenavComponent} from './components/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginViewComponent,
     NavbarComponent,
-    DashboardViewComponent
+    DashboardViewComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    SidenavComponent
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+// @ts-ignore
+export class AppModule {
+}
