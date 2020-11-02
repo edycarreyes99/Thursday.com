aafc8a94bf2888e3a879392241f5b290d14d6ca1
Edycar Reyes <edycarreyes@gmail.com>
2020-11-02 01:00:09 -0600

    Designed the both sidenav bars for navigate in the platform

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -8,12 +8,18 @@ import {Component, OnInit} from '@angular/core';
 export class SidenavComponent implements OnInit {
 
   sidenavElement: HTMLElement;
+  boardsElement: HTMLElement;
+  boardsBottomElement: HTMLElement;
+  toggleIconElement: HTMLElement;
 
   constructor() {
   }
 
   ngOnInit(): void {
     this.sidenavElement = document.getElementById('sidenav');
+    this.boardsElement = document.getElementById('boards-container');
+    this.boardsBottomElement = document.getElementById('boards-bottom');
+    this.toggleIconElement = document.getElementById('toggleIcon');
   }
 
   closeSideNav(): void {
@@ -22,6 +28,18 @@ export class SidenavComponent implements OnInit {
 
   openSideNav(): void {
     this.sidenavElement.style.width = '4.12rem';
+  }
+
+  toggleBoards(): void {
+    if (this.boardsElement.style.width === '0rem') {
+      this.boardsElement.style.width = '16rem';
+      this.boardsBottomElement.style.display = 'flex';
+      this.toggleIconElement.style.transform = 'rotate(180deg)';
+    } else {
+      this.boardsElement.style.width = '0rem';
+      this.boardsBottomElement.style.display = 'none';
+      this.toggleIconElement.style.transform = 'rotate(-180deg)';
+    }
   }
 
 }
